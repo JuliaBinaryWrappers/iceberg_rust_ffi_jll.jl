53d59cf656b54e205dd4be9c42453b9a04365493
jlbuild <juliabuildbot@gmail.com>
2025-12-01 15:24:44 +0000

    iceberg_rust_ffi_jll build 0.6.1+0

--- src/iceberg_rust_ffi_jll.jl
+++ src/iceberg_rust_ffi_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("iceberg_rust_ffi")
-JLLWrappers.@generate_main_file("iceberg_rust_ffi", UUID("6bd5c94f-693c-53e3-983d-a09fad412f22"))
+JLLWrappers.@generate_main_file("iceberg_rust_ffi", Base.UUID("6bd5c94f-693c-53e3-983d-a09fad412f22"))
 end  # module iceberg_rust_ffi_jll
